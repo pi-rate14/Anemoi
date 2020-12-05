de68d355928489b07b00fdb0861964ffe223efaa
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2020-12-05 07:10:40 +0530

    added custom calsses based on weather condition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const api = {
+    key: 'b3601570ed68feff1dfc217f9af9fde3',
+  }
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const [weatherClass, setWeatherClass] = useState('')
+
+  const changeWeatherClass = () => {
+    if (typeof weather.main != 'undefined') {
+      if (weather.weather[0].main === 'fog') {
+        setWeatherClass('fog')
+      } else if (weather.weather[0].main === 'hazy') {
+        setWeatherClass('hazy')
+      } else if (weather.weather[0].main === 'rain') {
+        setWeatherClass('rain')
+      } else if (weather.weather[0].main === 'snow') {
+        setWeatherClass('snow')
+      } else if (
+        weather.main.temp > 16 &&
+        weather.weather[0].main === 'clouds'
+      ) {
+        setWeatherClass('hotCloudy')
+      } else if (
+        weather.main.temp > 16 &&
+        weather.weather[0].main === 'clear'
+      ) {
+        setWeatherClass('hotClear')
+      } else if (
+        weather.main.temp < 16 &&
+        weather.weather[0].main === 'clouds'
+      ) {
+        setWeatherClass('coldCloudy')
+      }
+      if (weather.main.temp > 16 && weather.weather[0].main === 'clear') {
+        setWeatherClass('coldClear')
+      }
+      console.log(weatherClass)
+    }
+  }
+
+  const search = (evt) => {
+    if (evt.key === 'Enter') {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${api.key}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result)
+          changeWeatherClass()
+          console.log(weatherClass)
+        })
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ]
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React lmao
-        </a>
-      </header>
+    <div className={weatherClass}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="search..."
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != 'undefined' ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{weather.main.temp}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </main>
     </div>
   )
 }
